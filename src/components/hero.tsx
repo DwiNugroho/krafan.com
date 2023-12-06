d335d72aa8417a45300f905ceff4a1ddf277af6c
DwiNugroho <adwinugroho1@gmail.com>
2023-12-06 16:03:38 +0700

    feat: add emoji to hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <div className='my-12 flex flex-col items-center space-y-8 text-center md:my-28'>
       <h1 className='text-5xl font-extrabold text-foreground sm:text-6xl'>
-        Welcome to{' '}
+        Welcome to 🚀{' '}
         <span className='relative'>
           Krafan
           <CrossOutUnderline className='absolute inset-x-0 -bottom-3' />
