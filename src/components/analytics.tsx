802f00165c42a00817cc42a42b97bd0785c68b0f
dwinugroho <adwinugroho1@gmail.com>
2024-01-06 20:13:02 +0700

    :recycle: (blog) change image for new blog

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,7 +1,4 @@
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
-import Script from 'next/script'
-
-import { env } from '@/env'
 
 const Analytics = () => {
   if (process.env.NODE_ENV !== 'production') return null
@@ -9,11 +6,6 @@ const Analytics = () => {
   return (
     <>
       <VercelAnalytics />
-      <Script
-        async
-        data-website-id={env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
-        src={`${env.NEXT_PUBLIC_UMAMI_URL}/script.js`}
-      />
     </>
   )
 }
