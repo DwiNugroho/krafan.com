09787237cfafa9f3cdfb29d83f9af9969252922c
dwinugroho <adwinugroho1@gmail.com>
2024-12-01 01:43:48 +0700

    🐛 fix(nginx): nginx config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Blogs, GetInTouch, Hero, Projects } from '@/components/home'
 import { GridBackground } from '@/components/svg'
-import { env } from '@/env'
 import { getAllBlogPosts, getAllProjects } from '@/lib/mdx'
 
 export default function Home() {
@@ -11,7 +10,6 @@ export default function Home() {
     <>
       <GridBackground className='absolute right-0 top-0 h-full max-h-[1200px] w-full stroke-gray-900/10 [mask-image:radial-gradient(100%_100%_at_100%_0%,white,transparent)] dark:stroke-gray-100/10' />
       <div className='relative flex w-full flex-col gap-24 py-8 md:gap-40 md:py-12'>
-        <p>{env.NEXT_PUBLIC_GISCUS_CATEGORY}</p>
         <Hero />
         <Projects projects={projects} />
         <GetInTouch />
